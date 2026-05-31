2669e14a3865fa9e482b876727c5cadfedd648a6
jewel-opare <2425403001@live.gctu.edu.gh>
2026-05-31 21:45:51 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File:initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name:  Jewel Frimpong Opare
+# STUDENT ID: 2425403001
+# Email: 2425403001@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Jewel Frimpong Opare, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+# Prints my initials in large stylish form.
 
-
+    print()
+    print("...JFO")
+    print()
+    print("...........JJJJ........FFFFFFFFFFFF.........OOOOOOOOOO......")
+    print("...........JJJJ........FFFFFFFFFFFF........000OOOOOOOOO.....")
+    print("...........JJJJ........FFFF................OOOO....OOOO.....")
+    print("...........JJJJ........FFFF................OOOO....OOOO.....")
+    print("...........JJJJ........FFFFFFFFFFFF........000O....OOOO.....")
+    print("...........JJJJ........FFFFFFFFFFFF........OOOO....OOOO.....")
+    print("...........JJJJ........FFFF................OOOO....OOOO.....")
+    print("...JJJJ....JJJJ........FFFF................OOOO....OOOO.....")
+    print("...JJJJJJJJJJJ....**...FFFF...........**...OOOOOOOOOOOO...**")
+    print("....JJJJJJJJJ.....**...FFFF...........**....OOOOOOOOOO....**")
+    print()
 
 main()
